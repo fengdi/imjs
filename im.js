619152984937dd27f6305cea4d7e0f51b47f757b
tangoboy <137757831@qq.com>
2020-03-04 22:22:48 +0800

    原始名称存储模块

--- im.js
+++ im.js
@@ -17,19 +17,21 @@
   };
   const doc = document;
   const head = doc.getElementsByTagName("head")[0] || doc.documentElement;
-  const rfile = /[^\/]+?$/;
-  const isAbsolute = path => {
-    return /^(?:\/|@|\w+:)/.test(path);
-  };
+
   const currentScript = () => {
     return doc.currentScript;
   };
+  let uid = 0;
+  const uuid = () => {
+    return "m-" + (++uid).toString(32);
+  };
+  const rfile = /[^\/]+?$/;
   const normalize = path => {
     path = path.replace(/\\/g, "/").replace(/\/{2,}/g, "/");
     let parts = path.split("/"),
       absolute = isAbsolute(path),
       prefix = "";
-    if (absolute) prefix = parts.shift() + "//";
+    if (absolute) prefix = parts[0] ? parts.shift() + "//" : "";
     for (let i = 0; i < parts.length; ) {
       if (parts[i] === "..") {
         if (i > 0 && parts[i - 1] !== "..") parts.splice(--i, 2);
@@ -40,38 +42,68 @@
     }
     return prefix + parts.join("/");
   };
+  const isAbsolute = path => {
+    return /^(?:\/|\w+:)/.test(path);
+  };
   const dirname = path => {
-    if (!path.match(/\//)) {
-      return "./";
-    }
-    return path.replace(rfile, "");
+    return ("" + path).replace(rfile, "");
   };
   const filename = path => {
-    return (rfile.exec(path) || [""])[0];
+    return (rfile.exec("" + path) || [""])[0];
   };
   const realpath = (id, dir) => {
-    const cwd = require.cwd;
-    const a = doc.createElement("a");
+    dir = dir || "";
+    let url = "";
     if (rfile.test(id)) {
       id = /\.js$/.test(id) ? id : `${id}.js`;
     } else {
       id = id + require.default;
     }
-    a.href = normalize(
-      isAbsolute(id) ? id.replace(/^@\//, `${cwd}/`) : `${dir || cwd}/${id}`
-    );
 
-    return normalize(a.href);
+    if (isAbsolute(id)) {
+      url = id;
+    } else if (id[0] && id[0] in aliasRoots) {
+      url = replaceAliasRoots(id);
+    } else {
+      dir = replaceAliasRoots(dir);
+      url = dir ? `${dir}/${id}` : id;
+    }
+
+    return normalize(url);
   };
   const modules = {
     // 'a' : {id:'a', deps:[], factory:(function(){}), dirname, filename, export:null}
   };
-  
-  const define = ( ...args ) => {
+
+  const aliasRoots = {
+    get "/"() {
+      const loc = window.location;
+      return normalize(loc.origin + dirname(loc.pathname));
+    },
+    get "@"() {
+      let cwd = require.cwd;
+      if (isAbsolute(cwd)) {
+        return cwd;
+      } else {
+        return normalize(aliasRoots["/"] + (require.cwd || "./"));
+      }
+    }
+  };
+  const replaceAliasRoots = (url = "") => {
+    return url.replace(/^[\s\S]/, a => {
+      if (a in aliasRoots) {
+        return aliasRoots[a];
+      } else {
+        return a;
+      }
+    });
+  };
+  const define = (...args) => {
     const [id, deps, factory] = args;
     let i, d, f;
     const curScript = currentScript();
-    const moduleId = curScript && curScript.$moduleId;
+    const moduleId = curScript && curScript.$id;
+    const moduleUrl = curScript && curScript.$url;
     if (args.length == 1) {
       [i, d, f] = [moduleId, [], id];
     } else if (args.length == 2) {
@@ -81,22 +113,27 @@
     } else {
       throw new Error("Arguments error.");
     }
-    return modules[i] = {
+    return (modules[i] = modules[i] || {
       id: i,
+      uuid: uuid(),
+      url: moduleUrl,
       deps: d,
       factory: f,
-      dirname: dirname(i),
-      filename: filename(i),
+      dirname: dirname(moduleUrl),
+      filename: filename(moduleUrl),
       time: new Date().getTime()
-    };
+    });
   };
-  const loadScript = url => {
+  const loadScript = (id, dir) => {
+    let url = realpath(id, dir);
+
     return new Promise((resolve, reject) => {
       const script = doc.createElement("script");
       script.addEventListener(
         "load",
         e => {
-          resolve(modules[url])
+          resolve(modules[id]);
+          head && script && script.parentNode && head.removeChild(script);
         },
         { once: true }
       );
@@ -107,39 +144,47 @@
         },
         { once: true }
       );
-      script.$moduleId = url;
+      script.$id = id;
+      script.$url = url;
       script.src = url + (require.tag ? "?" + require.tag : "");
       head.insertBefore(script, head.firstChild);
     });
   };
   const hooks = {};
-  const setHook = (id, factory) =>{
-    return hooks[realpath(id)] = factory;
-  }
+  const setHook = (id, factory) => {
+    return (hooks[realpath(id)] = factory);
+  };
   const getHook = id => {
     const url = realpath(id);
-    return hooks[url] && define(id, [], hooks[url])
-  }
-  const require = async function(id, callback) {
+    return hooks[url] && define(id, [], hooks[url]);
+  };
 
-    const url = realpath(id);
-    
-    const module = modules[url] || await loadScript(url) || getHook(id);
+  const fetchModule = async (id, root, path = []) => {
+    const module = modules[id] || (await loadScript(id, root)) || getHook(id);
 
-    if (!module) throw new Error(`The module ${id} is not defined. You can define AMD module or use hook.`);
-    
+    if (!module)
+      throw new Error(
+        `The module ${id} is not defined. You can define AMD module or use hook.`
+      );
+    // path.push(id);
+
     const { deps = [], factory, dirname, filename } = module;
 
     if (!("export" in module)) {
       module.export = await factory.apply(
         { dirname, filename },
-        (await Promise.all(deps.map(did => require(realpath(did, dirname))))) ||
+        (await Promise.all(deps.map(did => fetchModule(did, dirname, path)))) ||
           []
       );
-      isFun(callback) && callback(module.export);
     }
-
     return module.export;
+  };
+
+  const require = async (id, callback) => {
+    id = id.trim();
+    let modulesExport = await fetchModule(id, require.cwd);
+    isFun(callback) && callback(modulesExport);
+    return modulesExport;
   };
 
   define.amd = true;
@@ -159,7 +204,7 @@
   require.main = "";
 
   require.setHook = setHook;
-  
+
   const curScript = currentScript();
   if (curScript) {
     try {
@@ -175,5 +220,5 @@
     }
   }
 
-  if (require.main) require(require.main);
+  if (!window.__IM_DONOT_AUTO_RUN__ && require.main) require(require.main);
 })(window);
